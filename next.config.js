38ffbb8fe8331df0a5bc63abab672bee5e89eac1
Shmul_D <126420344+ShmulD@users.noreply.github.com>
2025-01-02 11:48:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     images: {
       unoptimized: true
     },
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/NEWup2wp',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '/NEWup2wp',
+    trailingSlash: true,
 };
 
 module.exports = nextConfig;
