adbb1429dae7be871f370913e4d811d3b56edca7
Shmul_D <126420344+ShmulD@users.noreply.github.com>
2025-01-02 10:12:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/NEWup2wp',
-  assetPrefix: '/NEWup2wp/',
   images: {
     unoptimized: true
   }
 };
+
+// בודק אם אנחנו מריצים ב-GitHub Pages
+if (process.env.GITHUB_PAGES) {
+  nextConfig.basePath = '/NEWup2wp';
+  nextConfig.assetPrefix = '/NEWup2wp/';
+}
 
 module.exports = nextConfig;
